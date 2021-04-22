75b4eb2afd66eb4b23f14d4c33bbe96f10cf4602
Algeps <nik17052001@outlook.com>
2021-04-22 23:13:56 +0600

    add normal move first person
    Сделал местность и добавил нормальное управление от первого лица

---
+++ Project_Moskovka/Assets/Script_Person/CharacterControl.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterControl : MonoBehaviour
+{
+    public float _speed = 5, _jumpSpeed = 8, _gravity = 20, _speedShift = 10;
+    bool _isGround;
+
+    private Vector3 _moveDir = Vector3.zero;
+    private CharacterController controller;
+
+    // Start вызывается перед первым обновлением кадра
+    void Start()
+    {
+        //получаем компонент из персонажа
+        controller = GetComponent<CharacterController>();
+    }
+
+    // FixedUpdate - фиксированнное значение срабатывания функции
+    void FixedUpdate()
+    {
+        if (controller.isGrounded)
+        {
+            //присваиваем _moveDir вертикальные и горизонтальные значения с клавиатуры
+            _moveDir = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+            _moveDir = transform.TransformDirection(_moveDir);
+            _moveDir *= _speed;
+
+            /*if (Input.GetKeyDown(KeyCode.Z) && controller.isGrounded)
+            {
+                _moveDir *= _speedShift;
+            }
+            else
+            {
+                
+            }*/
+        }
+        
+
+
+        if ( Input.GetKeyDown(KeyCode.Space) && controller.isGrounded)
+        {
+            _moveDir.y = _jumpSpeed;
+        }
+
+        _moveDir.y -= _gravity * Time.deltaTime;
+
+        controller.Move(_moveDir * Time.deltaTime);
+    }
+}
---
+++ Project_Moskovka/Assets/Script_Person/MouseLook.cs
@@ -0,0 +1,73 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[AddComponentMenu("Camera-Control/MouseLook")]
+public class MouseLook : MonoBehaviour
+{
+    //выпадающий список для найстройки осей вращения
+    public enum RotationAxes { MouseXandY = 0, MouseX = 1, MouseY =2 };
+    public RotationAxes axes = RotationAxes.MouseXandY;
+    //перменные чувствительности мыши
+    public float sensivityX = 2;
+    public float sensivityY = 2;
+    //переменные максимального угла вращения X
+    public float minimumX = -360;
+    public float maxmimumX = 360;
+    //переменные максимального угла вращения Y
+    public float minimumY = -360;
+    public float maxmimumY = 360;
+    //переменные определяющий текущий уровень вращения
+    float rotationX = 0;
+    float rotationY = 0;
+    //переменная содержащая тип вращения "Quaternion"
+    Quaternion originalRotation;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (GetComponent<Rigidbody>())
+        {
+            GetComponent<Rigidbody>().freezeRotation = true;
+        }
+        originalRotation = transform.localRotation;
+    }
+
+    public static float ClampAngle (float angle, float min, float max)
+    {
+        if (angle < -360) angle += 360;
+        if (angle > 360) angle -= 360;
+        return Mathf.Clamp(angle, min, max);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(axes == RotationAxes.MouseXandY)
+        {
+            rotationX += Input.GetAxis("Mouse X") * sensivityX;
+            rotationY += Input.GetAxis("Mouse Y") * sensivityY;
+
+            rotationX = ClampAngle(rotationX, minimumX, maxmimumX);
+            rotationY = ClampAngle(rotationY, minimumY, maxmimumY);
+            Quaternion xQuaternion = Quaternion.AngleAxis(rotationX, Vector3.up);
+            Quaternion yQuaternion = Quaternion.AngleAxis(rotationY, -Vector3.right);
+            transform.localRotation = originalRotation * xQuaternion * yQuaternion;
+        }
+        else if (axes == RotationAxes.MouseX)
+        {
+            rotationX += Input.GetAxis("Mouse X") * sensivityX;
+            rotationX = ClampAngle(rotationX, minimumX, maxmimumX);
+            Quaternion xQuaternion = Quaternion.AngleAxis(rotationX, Vector3.up);
+            transform.localRotation = originalRotation * xQuaternion;
+        }
+        else if (axes == RotationAxes.MouseY)
+        {
+            rotationY += Input.GetAxis("Mouse Y") * sensivityY;
+            rotationY = ClampAngle(rotationY, minimumY, maxmimumY);
+            Quaternion yQuaternion = Quaternion.AngleAxis(-rotationY, Vector3.right);
+            transform.localRotation = originalRotation * yQuaternion;
+        }
+
+    }
+}
