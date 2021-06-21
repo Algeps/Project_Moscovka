3bbf43d4f0678645363a54165231518e9c5517c0
Algeps <nik17052001@outlook.com>
2021-06-21 10:07:56 +0600

    релиз 0.2 alpha

---
+++ Project_Moskovka/Assets/Menu/PauseMenu/PauseMenu.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+    public static bool GameIsPaused = false; 
+
+    public GameObject pauseMenuUI;
+
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            if(GameIsPaused)
+            {
+                Resume();
+            }
+            else
+            {
+                Pause();
+            }
+        }
+    }
+
+    public void Pause()
+    {
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        GameIsPaused = true;
+    }
+
+    public void Resume()
+    {
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;//можно использовать для слоумо
+        GameIsPaused = false;
+    }
+
+    public void BackMainMenu()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene("Main_Menu");
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+}
--- Project_Moskovka/Assets/Script_Person/HUD.cs
+++ Project_Moskovka/Assets/Script_Person/HUD.cs
@@ -4,29 +4,15 @@ using UnityEngine;
 
 public class HUD : MonoBehaviour
 {
-    public string _version = "0.1 alfa";
+    public string _version = "0.2 alfa";
     public Vector2 vVersion;
     public Font versionFont;
     public byte sizeText;
 
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void FixedUpdate()
-    {
-        
-    }
-
-    //������ �� ������
     void OnGUI ()
     {
         GUI.skin.font = versionFont;
         GUI.skin.label.fontSize = sizeText;
-        GUI.Label(new Rect(vVersion.x, vVersion.y, 250, 150), "version: "+_version);
+        GUI.Label(new Rect(vVersion.x, vVersion.y, 250, 150), "Version: "+_version);
     }
 }
